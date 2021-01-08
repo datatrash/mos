120bc95c4568aad545e0f6acb93542feca9c8489
Roy Jacobs <roy.jacobs@gmail.com>
2021-01-08 19:30:12 +0100

    Handle nested comments properly

--- src/parser/parse_tools.rs
+++ src/parser/parse_tools.rs
@@ -1,13 +1,13 @@
 use super::Span;
 use nom::branch::alt;
-use nom::bytes::complete::{is_not, tag, take_until, take_while_m_n};
+use nom::bytes::complete::{is_not, tag, take_until, take_while, take_while_m_n};
 use nom::character::complete::multispace0;
 use nom::character::complete::{char, multispace1};
 use nom::combinator::{map, map_res, value};
-use nom::error::ParseError;
+use nom::error::{Error, ErrorKind, ParseError};
 use nom::multi::{fold_many0, many0};
 use nom::sequence::{delimited, pair, preceded, tuple};
-use nom::IResult;
+use nom::{FindSubstring, IResult, InputLength, InputTake};
 
 fn is_hex_digit(c: char) -> bool {
     c.is_digit(16)
@@ -63,9 +63,29 @@ fn cpp_comment<'a, E: ParseError<Span<'a>>>(input: Span<'a>) -> IResult<Span<'a>
     })(input)
 }
 
+fn inside_c_comment<'a, E: ParseError<Span<'a>>>(
+    input: Span<'a>,
+) -> IResult<Span<'a>, Span<'a>, E> {
+    // Once we're inside a C comment, we don't care about anything except perhaps another /*
+    let (input, _) = take_until("/*")(input)?;
+    let (input, _) = tag("/*")(input)?;
+
+    // Found another /*, so now we either recurse or we go on until the matching */
+    let (input, _) = map(
+        value(
+            (),
+            pair(alt((inside_c_comment, take_until("*/"))), tag("*/")),
+        ),
+        |_| Span::new(""),
+    )(input)?;
+
+    // Ignore any trailing characters until we're up to the next */, so the outer function can deal with that
+    take_until("*/")(input)
+}
+
 fn c_comment<'a, E: ParseError<Span<'a>>>(input: Span<'a>) -> IResult<Span<'a>, Span<'a>, E> {
     map(
-        value((), tuple((tag("/*"), take_until("*/"), tag("*/")))),
+        value((), tuple((tag("/*"), inside_c_comment, tag("*/")))),
         |_| Span::new(""),
     )(input)
 }
@@ -117,7 +137,7 @@ mod tests {
 
     #[test]
     fn can_ignore_nested_c_comments() {
-        let input = Span::new("   foo /* he /* llo */   \n   bar");
+        let input = Span::new("   foo /* /*  /*hello */ */*/   \n   bar");
         let parser: IResult<Span, Vec<Span>> = many0(alt((ws(tag("foo")), ws(tag("bar")))))(input);
         let fragments: Vec<&str> = parser
             .unwrap()
